d2b24e7def5ec3c96e17bba1dcf1113d11ccef2a
TrevorCollins <tcollinst1i@gmail.com>
2025-07-10 14:50:10 -0500

    Fix import errors

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -2,13 +2,13 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 import { formatDate } from '@/lib/utils'
-import MDXContent from '@/components/mdx-content'
-import { ArrowLeftIcon } from '@radix-ui/react-icons'
-import { getProjectBySlug, getProjects } from '@/lib/projects'
+import MDXContent from '@/components/MDXContent'
+import { getFileBySlug, getFiles } from '@/lib/fetch'
 import { notFound } from 'next/navigation'
+import { ArrowLeftIcon } from 'lucide-react'
 
 export async function generateStaticParams() {
-  const projects = await getProjects()
+  const projects = await getFiles('projects')
   const slugs = projects.map(project => ({ slug: project.slug }))
 
   return slugs
@@ -20,7 +20,7 @@ export default async function Project({
   params: { slug: string }
 }) {
   const { slug } = params
-  const project = await getProjectBySlug(slug)
+  const project = await getFileBySlug(slug, 'projects')
 
   if (!project) {
     notFound()
@@ -30,11 +30,11 @@ export default async function Project({
   const { title, image, author, publishedAt } = metadata
 
   return (
-    <section className='pb-24 pt-32'>
+    <section className='pt-32 pb-24'>
       <div className='container max-w-3xl'>
         <Link
           href='/projects'
-          className='mb-8 inline-flex items-center gap-2 text-sm font-light text-muted-foreground transition-colors hover:text-foreground'
+          className='text-muted-foreground hover:text-foreground mb-8 inline-flex items-center gap-2 text-sm font-light transition-colors'
         >
           <ArrowLeftIcon className='h-5 w-5' />
           <span>Back to projects</span>
@@ -53,12 +53,12 @@ export default async function Project({
 
         <header>
           <h1 className='title'>{title}</h1>
-          <p className='mt-3 text-xs text-muted-foreground'>
+          <p className='text-muted-foreground mt-3 text-xs'>
             {author} / {formatDate(publishedAt ?? '')}
           </p>
         </header>
 
-        <main className='prose mt-16 dark:prose-invert'>
+        <main className='prose dark:prose-invert mt-16'>
           <MDXContent source={content} />
         </main>
       </div>

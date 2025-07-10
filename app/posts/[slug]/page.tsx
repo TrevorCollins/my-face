073716116c9ebd08a679b0ef963281dca9af7ab3
TrevorCollins <tcollinst1i@gmail.com>
2025-07-10 15:04:38 -0500

    Await params

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -14,8 +14,12 @@ export const generateStaticParams = async () => {
   return slugs
 }
 
-export default async function Post({ params }: { params: { slug: string } }) {
-  const { slug } = params
+export default async function Post({
+  params
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
   const post = await getFileBySlug(slug, 'posts')
   if (!post) {
     notFound()
